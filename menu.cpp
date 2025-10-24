b191d3a56a82f5737fd2aa6271aa802e20c3b37e
arina udina <arinaudina@MacBook-Air-arina.local>
2025-10-24 18:04:29 +0300

    added menu

---
+++ menu.cpp
@@ -0,0 +1,31 @@
+﻿#include <stdio.h>
+#include <locale.h>
+#include <time.h>
+#include <math.h>
+#define MAX_SIZE 200								//Максимальное количество элементов массива
+#define RAND_MAX 10000
+
+	// Функционал ввода данных в массив:
+
+
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+	int ansver, input_status=0;
+	double a[MAX_SIZE];
+
+	printf("Меню:\n");
+	printf("  1.Ввод\n  2.Вывод\n  3.");
+	scanf("%d", &ansver);
+
+	switch (ansver)
+	{
+		case 1:{
+			//input_status = input(a, MAX_SIZE);
+			break;
+		}
+
+	}
+}
+
+//Проецируемое меню для пользователя
