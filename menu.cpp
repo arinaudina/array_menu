b33ae839e84b4516bf86863596c4e2de19bab7e5
arina udina <arinaudina@MacBook-Air-arina.local>
2025-11-10 13:52:25 +0300

    redid the program completion

--- menu.cpp
+++ menu.cpp
@@ -2,50 +2,57 @@
 #include <locale.h>
 #include <time.h>
 #include <math.h>
-#define MAX_SIZE 200								//Максимальное количество элементов массива
+#define MAX_SIZE 200	
+#define MANUAL_LIMIT 20
+#define ERROR_ONE -2
+#define ERROR_TWO -1
+#define NO_ERROR 0
+
 
 // Обьявление всех подфункций меню:
 
-int input(double arr[], int size, int *error_remark); //Функция ввода
+int input(double arr[], int size, int manual_limit, int *error_remark);		 //Функция ввода
 
 
 int main()
 {
 	setlocale(LC_ALL, "Rus");
 
-	double array[MAX_SIZE];				// Массив, имеющий максимальный размер
-	int answer = 0;							// Переменная задающая ответ пользователя
-	int error_remark = 0;				// Переменная для записи и передачи ошибок из функций
+	double array[MAX_SIZE];				
+	int error = ERROR_ONE;	
+	int answer = 0;	
+	int input_status = 0;
 
-	do{
-		if(error_remark == 0) 			// проверка на наличие ошибки( чтоб не печатались пункты меню после ошибки пользователя ВНУТРИ функций)
-		{
+	do
+	{
+		if(error != ERROR_TWO)
 			printf("\nМеню:\n");
-			printf("\n  1.Ввод\n  2.Вывод\n  3.\n");
+			printf("\n  1.Ввод\n  2.Вывод\n  3.Сортировка\n  0.Выход\n");
 			printf("\nВведите пункт меню: ");
-		}
 
-		if((scanf("%d", &answer) !=1))  // проверка значения ввода (Сообщает об ошибках ДО и ПОСЛЕ входа в функции)
+		if((scanf("%d", &answer) !=1))
 		{
-			printf("ERROR\n");
-			return 1;
+			printf("\nERROR 2:\nПроизошла ошибка при вводе значений. Попробуйте запустить программу снова\n");
+			return -1;
 		}
-
 		switch (answer) 				// Обработка ответа пользователя
 		{
 			case 1:
-			{
-				int input_status = 0;		// Статус ввода массива (изначально - 0, значит массив не ввведен, в бущудем флаг для запрета использования других функций до введения массива)
-				input_status = input(array, MAX_SIZE, &error_remark); // Функция ввода массива, сохраняем размер массива и используем как статус ввода
+				input_status = input(array, MAX_SIZE, MANUAL_LIMIT, &error);
 
-				if ((error_remark == 0) && (input_status != 0))	// проверка на наличие ошибки( чтоб не печатались пункты меню после ошибки пользователя ВНУТРИ функций)
-				{
+				if ((error == NO_ERROR ) && (input_status > 0))
 					printf("\nВаш массив готов\n");
-				}
-				break;
-			}
-		};
-	} while (answer >0);
+				else if (error == ERROR_TWO)
+					printf("\nERROR 2:\n  Произошла ошибка при вводе значений. Попробуйте запустить программу снова\n");
 
-	return 0;
+				break;
+
+			case 0:
+				printf("\n...Выход...\nРабота завершена корректно\n");
+				return 0;
+		};
+
+	} while (error != ERROR_TWO);
+
+	return -1;
 }
